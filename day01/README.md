980a12b8a8b1682a43d0706d8017a32826512786
Akinola Ayodeji <akinayodeji4all@gmail.com>
2022-12-07 21:32:18 +0100

    AOC day 1

---
+++ day01/README.md
@@ -0,0 +1,4 @@
+# Day 01
+## Calorie counting 
+
+https://adventofcode.com/2022/day/1
---
+++ day01/main.py
@@ -0,0 +1,42 @@
+import os
+import bisect
+import sys
+
+def cal_calories_counting(values):
+    max_ef_calories = 0
+    current_ef_calories = 0
+    efs_calories = []
+    for value in values:
+        if value == '':
+            max_ef_calories = max(max_ef_calories, current_ef_calories)
+            current_ef_calories = 0
+            if max_ef_calories not in efs_calories:
+                bisect.insort(efs_calories, max_ef_calories) 
+            continue
+
+        if current_ef_calories != 0:
+            current_ef_calories += int(value)
+        else:
+            current_ef_calories = int(value)
+
+    return efs_calories
+
+def calorie_counting_part1(values):
+    efs_calories = cal_calories_counting(values)
+    return efs_calories[-1]
+
+
+def calorie_counting_part2(values):
+    efs_calories = cal_calories_counting(values)
+    return efs_calories[-3:]
+
+dir = os.path.abspath(os.path.dirname(__file__))
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        with open(path) as file:
+            _input = file.read().splitlines()
+            part1 = calorie_counting_part1(_input)
+            part2 = calorie_counting_part2(_input)
+            print('Part 1: ', part1)
+            print('Part 2:', part1)
+
